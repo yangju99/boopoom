80f0c031f0ff778d62e68cc6a51f427ac59676b4
yangju99 <didwndud3299@naver.com>
2026-03-05 15:20:28 +0900

    first commit

---
+++ src/main/java/com/example/boopoom/BoopoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.boopoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoopoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoopoomApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,1 @@
+create databse boopoom;
